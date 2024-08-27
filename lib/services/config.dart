7b36cfdbdf7779c95d72465e279169b10484fcd2
mehdinathani <mehdinathani@gmail.com>
2024-08-26 20:12:47 -0700

    web platform updated.

--- lib/services/config.dart
+++
@@ -1,5 +0,0 @@
-class GoogleApiConfig {
-  static const String apiKey = "AIzaSyBt1gmC8fpc_v74E7_l1HHDrtQ0lQhWh2I";
-  static const String SpreadsheetID =
-      '1pXPOPkZBS_2_8H6Q1vpsOy4ppMAR0kOKghJT15IeuQQ';
-}
